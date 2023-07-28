e6f8e0198aadfdb29a854e4b6231aa55efefb8b2
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-07-28 10:33:48 +0530

    Add files via upload

---
+++ Project_215/src/mq/java/classes/TestA.java
@@ -0,0 +1,7 @@
+package mq.java.classes;
+
+class TestA 
+{
+    //No modifier class
+	//This class information only can reusable with in package
+}
---
+++ Project_215/src/mq/java/classes/TestB.java
@@ -0,0 +1,9 @@
+package mq.java.classes;
+
+public class TestB 
+{
+	
+	//Class with public modifier
+	//public modifier class can reuse any where in the project
+	//[With in package or outside packages]
+}

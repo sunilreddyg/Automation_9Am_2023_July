475eac3157b63cfcddd3f54eda68c9014a693785
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-09-15 11:44:48 +0530

    Add files via upload

--- Project_215/src/library/Run_Using_Junit.java
+++ Project_215/src/library/Run_Using_Junit.java
@@ -4,6 +4,9 @@ import org.junit.Test;
 
 public class Run_Using_Junit extends CommonActions
 {
+	
+	
+	
 
 	@Test
 	public void test() 
@@ -12,6 +15,16 @@ public class Run_Using_Junit extends CommonActions
 		System.out.println(driver.getTitle());
 		typetext(CommonObjects.email, "Admin");
 		sendkeystoTextbox(CommonObjects.password, "admin@123");
+	}
+	
+	//@Test
+	public void dropdowntest()
+	{
+		setupchrome();
+		ClickonElement("//a[contains(.,'Sign Up')]");
+		selectdropdownoption("//select[@id='day']", "24");
+		selectdropdownoption("//select[@id='month']", "Dec");
+		select_dropdown("//select[@id='year']", "index", "20");
 	}
 
 }
